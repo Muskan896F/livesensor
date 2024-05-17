ac36c855e58899a9d3fe7f26e400a288f8e95280
Muskan986F <muskan17ara@gmail.com>
2024-05-17 14:58:22 +0530

    after edding e

---
+++ OneDrive/Desktop/SENSORLIVE/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+#from typing import List
+#from typing import List
+
+def get_requirements()->list[str]:
+    get_requirements = list[str] =[]
+    return get_requirements
+
+
+
+
+setup(
+name='Sensor',
+version="0.0.1",
+author="Muskan",
+author_email="muskan17ara@gmail.com",
+packages=find_packages(),
+    install_requires = get_requirements(), #S["pymongo"]
+    
+)
